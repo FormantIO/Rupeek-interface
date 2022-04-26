53c7650e5214b760f62cfd2fe3fc149a7c5390c6
alenjdev <alen@formant.io>
2022-04-26 09:32:53 -0500

    adpter

--- adapter/intervention_client.py
+++ adapter/intervention_client.py
@@ -1,4 +1,5 @@
 from formant.sdk.agent.v1 import Client as FormantClient
+from formant.sdk.cloud.v1 import Client as CloudClient
 import os
 
 
@@ -6,7 +7,8 @@ class InterventionClient:
     def __init__(
         self,
     ):
-        self.agent_url = os.getenv("AGENT_URL", "unix: // /var/lib/formant/agent.sock")
+        agent_url = "unix:///var/lib/formant/agent.sock"  # type: str
+        self.agent_url = os.getenv("AGENT_URL", agent_url)
         self._fclient = FormantClient(agent_url=self.agent_url, ignore_throttled=True)
         self.internvention_id = ""
 
@@ -15,6 +17,14 @@ class InterventionClient:
             "Assist Client"
         )
         self.internvention_id = intervention_response.id
+
+    def remove_self_from_queue(self):
+        params = {
+            "interventionId": self.internvention_id,
+            "interventionType": "teleop",
+            "data": {"state": "failure", "notes": "removed from queue"},
+        }
+        CloudClient.create_intervention_response(params)
 
     def run(self):
         self.create_intervention_request()
